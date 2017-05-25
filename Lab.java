ec13cdac37de2d4a7f3340721714b72c2e91986f
Ruftos <mrprolex@yandex.ru>
2017-05-25 21:49:53 +0300

    Add files via upload

---
+++ Lab.java
@@ -0,0 +1,10 @@
+public class Lab
+{	
+	public static void main (String [] args)
+	{	
+		String in="";
+		char result;
+		result = in.charAt (3);
+		System.out.println(result);
+	}
+}
---
+++ palindrome.java
@@ -0,0 +1,20 @@
+public class palindrome
+{
+	public static void main (String [] args)
+	{
+		String in="";
+		for (int count=0; count<args.length; count++)
+			{
+			in = args [count];
+			}
+
+		System.out.println("vvedite" + in);
+	}
+	public static Lab (String in)
+	{	
+		String in="";
+		char result;
+		result = in.charAt (3);
+		System.out.println(result);
+	}
+}
